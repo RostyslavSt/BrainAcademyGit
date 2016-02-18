baa91f0f1703d05fb79ffe825e3160ada68af759
Student <student@brain.local>
2016-02-18 21:35:03 +0200

    fgfg322

--- Lab_works/6_Practise_13_02_2016/Module 3/Bootstrap/Template/js/base.js
+++ Lab_works/6_Practise_13_02_2016/Module 3/Bootstrap/Template/js/base.js
@@ -1,5 +1,10 @@
 (function () {
   "use strict";
 
-   var swiper = new Swiper('.swiper-container');  
-})();
+   var swiper = new Swiper('.swiper-container', {  
+   	slidesPerview: 1,
+   	 loop: true,
+   	// autoplay: 3000,
+   	// autoplay: true
+   });
+}) ();
